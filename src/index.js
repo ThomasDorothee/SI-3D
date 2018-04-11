0790b9ce991e5280100402edcfa43b14f47b9586
tomdoro <thomas_051998@outlook.fr>
2018-04-11 10:58:10 +0200

    Map V0.1 🚀

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'flexboxgrid';
 import './styles/styles.scss';
+
+var freeBox = document.querySelector('.chosenOne');
+
+freeBox.addEventListener('click', function () {
+  alert('THE CHOSEN ONE');
+});
