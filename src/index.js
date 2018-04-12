198b9f22e6756dc7f2889ea6351f2b674220b1d0
tomdoro <thomas_051998@outlook.fr>
2018-04-12 15:11:07 +0200

    BUG

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import 'flexboxgrid';
 import './styles/styles.scss';
-
-var freeBox = document.querySelectorAll('.free');
-var chosen = document.querySelector('.chosenOne');
-
-
-chosen.addEventListener('click', function () {
-  alert('THE CHOSEN ONE');
-});
---
+++ src/salon.js
@@ -0,0 +1,7 @@
+var freeBox = document.querySelectorAll('.free');
+var chose = document.querySelector('.test');
+
+
+chose.addEventListener('click', function () {
+  alert('THE CHOSEN ONE');
+});
